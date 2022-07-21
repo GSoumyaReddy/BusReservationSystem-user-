d5046378e673e1d830dc3eab7c6065250dc446c2
GSoumyaReddy <106742830+GSoumyaReddy@users.noreply.github.com>
2022-07-21 17:15:34 +0530

    Add files via upload

--- IUserRepository.java
+++ IUserRepository.java
@@ -1,13 +1,12 @@
-package com.cg.Repository;
+package com.cg.repository;
 
-import java.util.List;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import com.cg.entity.User;
 
-@Repository
 public interface IUserRepository extends JpaRepository<User,Integer>{
 	
 	
